eeffb2d4c4d6467c53f38e12a332096779d7c8a2
Eng. Oduor Felix <oduorfelix2@gmail.com>
2023-06-05 15:01:21 +0300

    :tada: #comment Initial commit

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Jokes Web App</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
